d9409c349e012a85928f30939dd3cf049eac2f69
pyollivthamizh <pyoliivethamizharasy@gmail.com>
2026-01-10 16:38:23 +0530

    flights

---
+++ pyolliveFlight/src/App.tsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+import './App.css'
+import SelectDeparture from './Flights/SelectDeparture';
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-white">
+        Tailwind is working
+      </h1>
+      <SelectDeparture />
+    </>
+  )
+}
+
+export default App
---
+++ pyolliveFlight/src/Flights/SelectDeparture.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Button} from '@/components/ui/button';
+import { Input } from '@/components/ui/Input';
+import "tailwindcss";
+
+const SelectDeparture = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default SelectDeparture;
